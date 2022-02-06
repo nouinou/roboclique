c54a2713afd8aea6c64bb75c06789e20e33d5f52
Hamza Nouinou <nouinouhamza@gmail.com>
2022-02-06 16:08:32 +0100

    feat: add tailwindcss

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,7 @@
-import './Header.css';
-
 const Header = () => {
 	return (
-		<div className="header robo-text">
-			<h1>Robo Clique</h1>
+		<div className="header flex flex-col items-center justify-center text-white robo-text w-screen min-h-20 bg-primary p-5 mb-6">
+			<h1 className="robo-text text-4xl md:text-7xl">Robo Clique</h1>
 		</div>
 	);
 };
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#3E4451',
+				accent: '#282C34',
+			},
+			minWidth: {
+				300: '300px',
+			},
+			minHeight: {
+				20: '20vh',
+			},
+		},
+	},
+	plugins: [],
+};
