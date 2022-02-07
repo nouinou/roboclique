1ae94551f41dc43cb7df32a06b3e7a4685b9d8d8
Hamza Nouinou <nouinouhamza@gmail.com>
2022-02-07 04:12:25 +0100

    refactor: header as pure component

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -1,12 +1,17 @@
+import { PureComponent } from 'react';
 import SearchBox from '../search-box/SearchBox';
 
-const Header = ({ searchTermChanged }) => {
-	return (
-		<div className="header flex flex-col items-center justify-center w-screen min-h-20 bg-primary p-5 mb-6">
-			<h1 className="robo-text text-5xl text-white md:text-7xl">Robo Clique</h1>
-			<SearchBox searchChange={searchTermChanged} />
-		</div>
-	);
-};
+class Header extends PureComponent {
+	render() {
+		return (
+			<div className="header flex flex-col items-center justify-center w-screen min-h-20 bg-primary p-5 mb-6">
+				<h1 className="robo-text text-5xl text-white md:text-7xl">
+					Robo Clique
+				</h1>
+				<SearchBox searchChange={this.props.searchTermChanged} />
+			</div>
+		);
+	}
+}
 
 export default Header;
